4c42f3c1df1e74cdb26de790f5e9cdaa1f20e8e8
Dylan <d.r.debie@student.utwente.nl>
2016-01-25 14:18:45 +0100

    added some changes to server and client. but not finished yet.

--- src/ss/qwirkle/network/Client.java
+++ src/ss/qwirkle/network/Client.java
@@ -7,16 +7,25 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.List;
+
+import nl.utwente.ewi.qwirkle.net.IProtocol;
+import ss.qwirkle.common.Move;
+import ss.qwirkle.common.tiles.Tile;
+/**
+ * 
+ * @author Dylan
+ *
+ */
 
 public class Client extends Thread {
 	
-	private String clientName;
 	private Socket sock;
 	private BufferedReader in;
 	private BufferedWriter out;
 	
-	public Client(String name, InetAddress host, int port) throws IOException {
-		clientName = name; 
+	public Client(InetAddress host, int port) throws IOException {
 		try {
 			sock = new Socket(host, port);
 	    	in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
@@ -25,9 +34,6 @@ public class Client extends Thread {
             System.out.println("ERROR: could not create a socket on " + host
                     + " and port " + port);
 		}	
-	}
-	public String getClientName() {
-		return clientName;
 	}
 	/** send a message to a ClientHandler. */
 	public void sendMessage(String message) {
@@ -42,7 +48,6 @@ public class Client extends Thread {
     }
 	/** close the socket connection. */
 	public void shutdown() {
-		print("Closing socket connection...");
 		try {
 			sock.close();
 			in.close();
@@ -52,7 +57,73 @@ public class Client extends Thread {
 			e.printStackTrace();
 		}	
 	}
-	private static void print(String message) {
-		System.out.println(message);
+	public String readMessage(String message) {
+		String response = null;
+		try {
+			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+			response = input.readLine();
+		} catch (IOException e) {
+		}
+		return (response == null) ? "" : response;
+	}
+	public void sendMove(Move m) {
+		String message = IProtocol.CLIENT_MOVE_PUT;
+		for (Tile t : m.getTiles()) {
+			message += String.format(" %d@%d,%d", t.toInt(), t.getX(), t.getY());
+		}
+		send(message);
+	}
+	public void tradeTile(int amount) {
+		String message = IProtocol.SERVER_MOVE_TRADE;
+		message += " " + amount;
+		send(message);
+	}
+	public void tradeMove(List<Tile> tiles) {
+		String message = IProtocol.CLIENT_MOVE_TRADE;
+		for (Tile t: tiles) {
+			message += " " + t.toInt();
+		}
+		send(message);
+	}
+	public void queuePlayer(List<Integer> numbers) {
+		String message = IProtocol.CLIENT_QUEUE;
+		message += " ";
+		Iterator<Integer> iterator = numbers.iterator();
+		while (iterator.hasNext()) {
+			message += iterator.next();
+			if (iterator.hasNext()) {
+				message += ",";
+			}
+		}
+		send(message);
+	}
+	public void quitPlayer() {
+		String message = IProtocol.CLIENT_QUIT;
+		send(message);
+	}
+	public void identifyPlayer(String name, List<IProtocol.Feature> features) {
+		String message = IProtocol.CLIENT_IDENTIFY;
+		message += " ";
+		Iterator<IProtocol.Feature> iterator = features.iterator();
+		while (iterator.hasNext()) {
+			message += iterator.next();
+			if (iterator.hasNext()) {
+				message += ",";
+			}
+		}
+		send(message);
+	}
+	
+	private void send(String message) {
+		try {
+			out.write(message);
+			out.newLine();
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void parseCommands() {
+		//startGame();
 	}
 }
